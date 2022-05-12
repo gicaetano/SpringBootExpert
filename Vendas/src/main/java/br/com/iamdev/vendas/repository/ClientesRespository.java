895f03df02c1de2924ad692cce859c3215502a9a
gielesig.gi <gielesig.gi@gmail.com>
2022-05-11 21:16:09 -0300

    First Commit

---
+++ Vendas/src/main/java/br/com/iamdev/vendas/repository/ClientesRespository.java
@@ -0,0 +1,15 @@
+package br.com.iamdev.vendas.repository;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.iamdev.vendas.model.Cliente;
+
+@Repository
+public class ClientesRespository {
+
+	public void persistir(Cliente cliente) {
+		// Acessa a base e salva o Cliente
+		
+	}
+
+}
