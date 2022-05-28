32390b9ff867f9639c57efed73700f98e260bc95
gielesig.gi <gielesig.gi@gmail.com>
2022-05-28 20:57:22 -0300

    salva e recupera clientes

--- vendas/src/main/java/com/iamdev/elesig/VendasApplication.java
+++ vendas/src/main/java/com/iamdev/elesig/VendasApplication.java
@@ -1,10 +1,31 @@
 package com.iamdev.elesig;
 
+import java.util.List;
+
+import org.hibernate.internal.build.AllowSysOut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.iamdev.elesig.domain.entity.Cliente;
+import com.iamdev.elesig.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
+	
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+			clientes.salvar(new Cliente("Douglas"));
+			clientes.salvar(new Cliente("Outro Cliente"));
+			clientes.salvar(new Cliente("Mais um Cliente"));
+			
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- vendas/src/main/java/com/iamdev/elesig/domain/entity/Cliente.java
+++ vendas/src/main/java/com/iamdev/elesig/domain/entity/Cliente.java
@@ -5,6 +5,20 @@ public class Cliente {
 	private Integer id;
 	private String nome;
 	
+	public Cliente() {
+	}
+	
+	public Cliente(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -17,7 +31,15 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "Cliente {" +
+				"id=" + id + 
+				", nome='" + nome + '\''+
+				'}';
+	}
+	
+	
+	
 }
