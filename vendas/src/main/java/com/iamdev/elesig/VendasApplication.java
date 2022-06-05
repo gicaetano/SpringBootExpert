5cf5fa4e720e0c2f09600f187820f1be967b2894
gielesig.gi <gielesig.gi@gmail.com>
2022-06-04 22:23:21 -0300

    mapeamento com Spring Data

--- vendas/src/main/java/com/iamdev/elesig/VendasApplication.java
+++ vendas/src/main/java/com/iamdev/elesig/VendasApplication.java
@@ -1,7 +1,5 @@
 package com.iamdev.elesig;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,35 +16,13 @@ public class VendasApplication {
 	public CommandLineRunner init(@Autowired ClienteRepository clientes) {
 		return args -> {
 			System.out.println("Salvando Clientes");
-			clientes.salvar(new ClienteEntity("Douglas"));
-			clientes.salvar(new ClienteEntity("Outro Cliente"));
+			clientes.save(new ClienteEntity("Douglas teste"));
+			clientes.save(new ClienteEntity("Outro Cliente"));
 
-			List<ClienteEntity> todosClientes = clientes.obterTodos();
-			todosClientes.forEach(System.out::println);
+			boolean existe = clientes.existsByNome("Douglas");
+			System.out.println("existe cliente com nome Douglas = " + existe);
 
-			System.out.println("Atualizando Clientes");
-			todosClientes.forEach(c -> {
-				c.setNome(c.getNome() + " atualizado.");
-				clientes.atualizar(c);
-			});
-
-			todosClientes = clientes.obterTodos();
-			todosClientes.forEach(System.out::println);
-
-			System.out.println("Buscando Clientes");
-			clientes.buscarPorNome("Cli").forEach(System.out::println);
-
-			System.out.println("Deletando Clientes");
-			clientes.obterTodos().forEach(c -> {
-				clientes.deletar(c);
-			});
-
-			todosClientes = clientes.obterTodos();
-			if (todosClientes.isEmpty()) {
-				System.out.println("Nenhum cliente encontrado.");
-			} else {
-				todosClientes.forEach(System.out::println);
-			}
+			
 		};
 	}
 
--- vendas/src/main/java/com/iamdev/elesig/domain/repository/ClienteRepository.java
+++ vendas/src/main/java/com/iamdev/elesig/domain/repository/ClienteRepository.java
@@ -1,75 +1,16 @@
 package com.iamdev.elesig.domain.repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.iamdev.elesig.domain.entity.ClienteEntity;
 
-@Repository
-public class ClienteRepository {
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Integer> {
 
-	@Autowired
-	private EntityManager entityManager;
+	List<ClienteEntity> findByNomeLike(String nome);
 	
-	@Transactional
-	public ClienteEntity salvar(ClienteEntity cliente) {
-		entityManager.persist(cliente);
-		return cliente;
-	}
+	boolean existsByNome(String nome);
 	
-	@Transactional
-	public ClienteEntity atualizar(ClienteEntity cliente) {
-		entityManager.merge(cliente);
-		return cliente;
-	}
-	
-	@Transactional
-	public void deletar(ClienteEntity cliente) {
-		if(!entityManager.contains(cliente)) {
-			cliente = entityManager.merge(cliente);
-		}
-		entityManager.remove(cliente);
-	}
-	
-	@Transactional
-	public void deletar(Integer id) {
-		ClienteEntity cliente = entityManager.find(ClienteEntity.class, id);
-		deletar(cliente);
-	}
-	
-	@Transactional(readOnly = true)
-	public List<ClienteEntity> buscarPorNome(String nome) {
-		String jpql = "select c from ClienteEntity c where c.nome like :nome ";
-		TypedQuery<ClienteEntity> query = entityManager.createQuery(jpql, ClienteEntity.class);
-		query.setParameter("nome", "%" + nome + "%");
-		return query.getResultList();
-	}
-	
-	public List<ClienteEntity> obterTodos() {
-		return entityManager
-				.createQuery("from ClienteEntity", ClienteEntity.class)
-				.getResultList();			
-}
-
-	private RowMapper<ClienteEntity> obterClienteMapper() {
-		return new RowMapper<ClienteEntity>() {
-			@Override
-			public ClienteEntity mapRow(ResultSet resultSet, int i) throws SQLException {
-				Integer id = resultSet.getInt("id");
-				String nome = resultSet.getString("nome");
-				return new ClienteEntity(id, nome);
-			}
-		};
-	}
 }
 
